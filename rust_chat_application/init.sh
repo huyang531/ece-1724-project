0e11f4cd46020e13c3512f3a4cc36112c2fd65a1
Yang Hu <yanghuu531@gmail.com>
2024-12-13 21:56:02 -0500

    add init script
    Signed-off-by: Yang Hu <yanghuu531@gmail.com>

---
+++ rust_chat_application/init.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Base URL for the API
+BASE_URL="http://127.0.0.1:3000/api/chatrooms"
+
+# Function to create a chat room
+create_chat_room() {
+    local user_id=$1
+    local room_name=$2
+    curl -H "Content-Type: application/json" -d "{\"user_id\":${user_id},\"room_name\":\"${room_name}\"}" ${BASE_URL}
+    echo
+}
+
+# Create 3 users
+curl -H "Content-Type: application/json" -d "{\"username\":\"World\",\"email\":\"lll@gmail.com\",\"password\":\"11111\"}" http://127.0.0.1:3000/api/user/signup
+curl -H "Content-Type: application/json" -d "{\"username\":\"Hello\",\"email\":\"yyy@gmail.com\",\"password\":\"11111\"}" http://127.0.0.1:3000/api/user/signup
+curl -H "Content-Type: application/json" -d "{\"username\":\"Hey..\",\"email\":\"zzz@gmail.com\",\"password\":\"11111\"}" http://127.0.0.1:3000/api/user/signup
+
+# Create 3 Chat Rooms
+create_chat_room 1 "ChatRoom1"
+create_chat_room 2 "ChatRoom2"
+create_chat_room 3 "ChatRoom3"
