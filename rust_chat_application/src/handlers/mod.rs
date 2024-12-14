65fb3e0a392a896eee54be6e3aaea92b22b5127a
Yang Hu <yanghuu531@gmail.com>
2024-12-14 00:54:23 -0500

    add basic ws and channeling feature; add launch.json
    Signed-off-by: Yang Hu <yanghuu531@gmail.com>

--- rust_chat_application/src/handlers/mod.rs
+++ rust_chat_application/src/handlers/mod.rs
@@ -1,4 +1,3 @@
 pub mod chat_room_apis;
-
 pub mod user_auth_apis;
-
+pub mod websocket_handler;
--- rust_chat_application/src/main.rs
+++ rust_chat_application/src/main.rs
@@ -1,18 +1,37 @@
 use axum::{
-    routing::{get, post},
+    routing::{any, get, post},
     Router
 };
-use tower_http::cors::{CorsLayer, Any};
-use std::net::SocketAddr;
+use tokio::sync::broadcast;
+use tower_http::{
+    cors::{CorsLayer, Any},
+    add_extension::AddExtensionLayer,
+};
+use std::{collections::HashMap, net::SocketAddr, sync::{Arc, Mutex}};
 use mysql_async::Pool;
 use crate::database::initialize_database;
 mod handlers;
 use crate::handlers::chat_room_apis::*;
 use crate::handlers::user_auth_apis::*;
+use crate::handlers::websocket_handler::ws_handler;
 mod services;
 mod repository;
 mod database;
 
+pub type ChatChannels = Arc<Mutex<HashMap<i32, (broadcast::Sender<(String, SocketAddr)>, broadcast::Receiver<(String, SocketAddr)>)>>>;
+
+#[derive(Clone)]
+pub struct AppState {
+    pub chat_channels: ChatChannels,
+}
+
+impl AppState {
+    pub fn new() -> Self {
+        AppState {
+            chat_channels: Arc::new(Mutex::new(HashMap::new())),
+        }
+    }
+}
 
 #[tokio::main]
 async fn main() {
@@ -37,6 +56,8 @@ async fn main() {
         .route("/api/user/login", post(user_login))
         .route("/api/user/logout", post(user_logout))
         .route("/api/user/fetch_status", post(fetch_user_status))
+        .route("/ws/{chat}", any(ws_handler))
+        .layer(AddExtensionLayer::new(AppState::new()))
         .layer(
             CorsLayer::new()
                 .allow_origin(Any)
@@ -44,13 +65,13 @@ async fn main() {
                 .allow_headers(Any),
         );
 
-        let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
-        println!("Server running on http://{}", addr);
-        
-        let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
-        axum::serve(listener, app).await.unwrap();
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    println!("Server running on http://{}", addr);
+    
+    let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
+    axum::serve(listener, app.into_make_service_with_connect_info::<SocketAddr>()).await.unwrap();
 
-    }
+}
 
 // Root handler
 async fn root() -> &'static str {
